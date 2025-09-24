aba59d23ed527860ee530a29690929d390feb65d
Harshit__07 <harshitsinghgla2415@gmail.com>
2025-09-24 10:39:26 +0530

    Create Sum_1D_Arr.java

---
+++ Sum_1D_Arr.java
@@ -0,0 +1,12 @@
+public class Sum_1D_Arr {
+    public static void main(String[]args){
+        int [] arr = {1,2,3,4,56,8,9};
+        int sum =0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+
+        }
+        System.out.print("The sum of the array is: "+sum);
+    }
+    
+}
